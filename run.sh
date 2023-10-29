e412a146e1955ef5d99d10568d5ae54dc0a5721f
zic <git@zicstardust.com>
2023-10-29 10:46:27 -0400

    fix errors

--- run.sh
+++ run.sh
@@ -1,18 +1,50 @@
-export KEYS_DIR="/etc/secureboot-keys"
-export MOKCONFIG="mokconfig.cnf"
-export KEY_NAME="MOKKERNEL"
-export CONTROL="${KEYS_DIR}/continue"
-sudo su
+#!/usb/bin/env bash
+MOK_DIRECTORY="/etc/secureboot-keys"
+MOKCONFIG="mokconfig.cnf"
+MOK_CERT_NAME="secureboot"
 
-if [ -f "$CONTROL" ]; then
+is_root=$(whoami)
+if [ ${is_root} != "root" ]; then
+	echo "run as root"
+	exit 2
+fi
+
+mkdir -p ${MOK_DIRECTORY}
+cat > ${MOK_DIRECTORY}/${MOKCONFIG} <<MOKCONF
+# This definition stops the following lines failing if HOME isn't
+# defined.
+HOME                    = .
+RANDFILE                = \$ENV::HOME/.rnd
+[ req ]
+distinguished_name      = req_distinguished_name
+x509_extensions         = v3
+string_mask             = utf8only
+prompt                  = no
+
+[ req_distinguished_name ]
+countryName             = US
+stateOrProvinceName     = nothing
+localityName            = nothing
+0.organizationName      = nothing
+commonName              = Secure Boot Signing Key
+emailAddress            = nothing@nothing.com
+
+[ v3 ]
+subjectKeyIdentifier    = hash
+authorityKeyIdentifier  = keyid:always,issuer
+basicConstraints        = critical,CA:FALSE
+extendedKeyUsage        = codeSigning,1.3.6.1.4.1.311.10.3.6
+nsComment               = "OpenSSL Generated Certificate"
+MOKCONF
+
 cat > /etc/kernel/postinst.d/00-signing <<HOOK
 #!/bin/usr/env bash
 
 set -e
 
 KERNEL_IMAGE="\$2"
-MOK_CERT_NAME="${KEY_NAME}"
-MOK_DIRECTORY="${KEYS_DIR}"
+MOK_CERT_NAME="${MOK_CERT_NAME}"
+MOK_DIRECTORY="${MOK_DIRECTORY}"
 
 if [ "$#" -ne "2" ] ; then
 	echo "Wrong count of command line arguments. This is not meant to be called directly." >&2
@@ -51,47 +83,15 @@ fi
 echo "Signing \$KERNEL_IMAGE..."
 sbsign --key "\$MOK_DIRECTORY/\$MOK_CERT_NAME.priv" --cert "\$MOK_DIRECTORY/\$MOK_CERT_NAME.pem" --output "\$KERNEL_IMAGE" "\$KERNEL_IMAGE"
 HOOK
-	chown root:root /etc/kernel/postinst.d/00-signing
-	chmod u+rx /etc/kernel/postinst.d/00-signing
-	rm -f ${CONTROL}
-else
-	mkdir -p ${KEYS_DIR}
-	cat > ${KEYS_DIR}/${MOKCONFIG} <<KEYS
-	# This definition stops the following lines failing if HOME isn't
-	# defined.
-	HOME                    = .
-	RANDFILE                = \$ENV::HOME/.rnd
-	[ req ]
-	distinguished_name      = req_distinguished_name
-	x509_extensions         = v3
-	string_mask             = utf8only
-	prompt                  = no
-	" >> mokconfig.cnf
-	[ req_distinguished_name ]
-	countryName             = BR
-	stateOrProvinceName     = Nada
-	localityName            = Nada
-	0.organizationName      = nenhuma
-	commonName              = Secure Boot Signing Key
-	emailAddress            = nada@nenhuma.com
-	
-	[ v3 ]
-	subjectKeyIdentifier    = hash
-	authorityKeyIdentifier  = keyid:always,issuer
-	basicConstraints        = critical,CA:FALSE
-	extendedKeyUsage        = codeSigning,1.3.6.1.4.1.311.10.3.6
-	nsComment               = "OpenSSL Generated Certificate"
-KEYS
-	chmod +x ${KEYS_DIR}/${MOKCONFIG}
-	openssl req -config ${KEYS_DIR}/${MOKCONFIG} -new -x509 -newkey rsa:2048 -nodes -days 36500 -outform DER -keyout "${KEYS_DIR}/${KEY_NAME}.priv" -out "${KEYS_DIR}/${KEY_NAME}.der"
-	openssl x509 -in ${KEYS_DIR}/${KEY_NAME}.der -inform DER -outform PEM -out ${KEYS_DIR}/${KEY_NAME}.pem
-	mokutil --import ${KEYS_DIR}/${KEY_NAME}.der   
-	echo "Run script again after reboot"
-	touch ${CONTROL}
-	sleep 5
-	reboot
-fi
 
+chown root:root /etc/kernel/postinst.d/00-signing
+chmod u+rx /etc/kernel/postinst.d/00-signing
+chmod +x ${MOK_DIRECTORY}/${MOKCONFIG}
 
+openssl req -config ${MOK_DIRECTORY}/${MOKCONFIG} -new -x509 -newkey rsa:2048 -nodes -days 36500 -outform DER -keyout "${MOK_DIRECTORY}/${MOK_CERT_NAME}.priv" -out "${MOK_DIRECTORY}/${MOK_CERT_NAME}.der"
+openssl x509 -in ${MOK_DIRECTORY}/${MOK_CERT_NAME}.der -inform DER -outform PEM -out ${MOK_DIRECTORY}/${MOK_CERT_NAME}.pem
+mokutil --import ${MOK_DIRECTORY}/${MOK_CERT_NAME}.der   
 
-
+echo "Reboot in 10 seconds"
+sleep 10s
+reboot
